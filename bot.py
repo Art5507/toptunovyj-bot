4d958fc3cff20218b231808dcc5e40fdb6370b60
Art5507 <arturbakiev028@gmail.com>
2024-06-13 21:31:43 +0500

    Тут много изменений, читай описание
    Команды для модерации - /ban, /timeout, /kick, /warn, /clear. Изменения в AFK-командах - вы можете оставить записку. Появились новые команды /tuck и /hug.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 import config
 import random
+from datetime import timedelta
 
 class aclient(discord.Client):
     def __init__(self):
@@ -66,43 +67,106 @@ async def self(interaction: discord.Interaction):
     await interaction.response.send_message(f"{random_number}")
 
 @tree.command(name = "afk", description = "Перейти в AFK режим")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} ушел в афк")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} ушел в афк: {сообщение}")
 
 @tree.command(name = "gn", description = "Перейти в режим сна")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} ушел спать :bed:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} ушел спать: {сообщение}")
 
 @tree.command(name = "brb", description = "Перейти в режим скоро вернусь")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} скоро вернется :hourglass_flowing_sand:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} скоро вернется: {сообщение}")
 
 @tree.command(name = "poop", description = "Перейти в режим какания")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас какает :toilet:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} сейчас какает: {сообщение}")
 
 @tree.command(name = "shower", description = "Перейти в режим принятия душа")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} принимает душ :shower:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} принимает душ: {сообщение}")
 
 @tree.command(name = "food", description = "Перейти в режим приема пищи")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас ест :bento:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} сейчас ест: {сообщение}")
 
 @tree.command(name = "lurk", description = "Перейти в режим скрытности")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас скрывается :busts_in_silhouette:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} сейчас скрывается: {сообщение}:")
 
 @tree.command(name = "work", description = "Перейти в рабочий режим")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} работает :briefcase:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} работает: {сообщение}")
 
 @tree.command(name = "study", description = "Перейти в режим обучения")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас учится :books:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} сейчас учится: {сообщение}")
 
 @tree.command(name = "nap", description = "Перейти в режим дремоты")
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас дремлет :sleeping:")
+async def self(interaction: discord.Interaction, сообщение:str):
+    await interaction.response.send_message(f"{interaction.user.mention} сейчас дремлет: {сообщение}")
+
+@tree.command(name = "tuck", description = "Уложить человека спать")
+async def self(interaction: discord.Interaction, участник:discord.Member, *, смайлик:str):
+    await interaction.response.send_message(f"Вы уложили {участник.mention} спать {смайлик} :point_right: :bed:")
+
+@tree.command(name = "hug", description = "Обнять человека")
+async def self(interaction: discord.Interaction, участник:discord.Member):
+    await interaction.response.send_message(f"{interaction.user.mention} обнимает {участник.mention} :hugging:")
+
+@tree.command(name = "clear", description = "Очистить чат")
+@app_commands.default_permissions(manage_messages=True)
+async def self(interaction: discord.Interaction, количество:int):
+    await interaction.channel.purge(limit=количество)
+    embed = discord.Embed(
+    colour=discord.Colour.teal(),
+    description= f":white_check_mark: Удалено **{количество}** сообщений"
+)
+
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
+@tree.command(name = "kick", description = "Выгнать участника")
+@app_commands.default_permissions(kick_members=True)
+async def self(interaction: discord.Interaction, участник:discord.Member, *, причина:str):
+    await участник.kick(reason=причина)
+    embed = discord.Embed(
+    colour=discord.Colour.teal(),
+    description= f":white_check_mark: **{участник}** был выгнан по причине: `{причина}`"
+)
+
+    await interaction.response.send_message(embed=embed)
+
+@tree.command(name = "ban", description = "Забанить участника")
+@app_commands.default_permissions(ban_members=True)
+async def self(interaction: discord.Interaction, участник:discord.Member, *, причина:str):
+    await участник.ban(reason=причина, delete_message_days=1)
+    embed = discord.Embed(
+    colour=discord.Colour.teal(),
+    description= f":white_check_mark: **{участник}** был забанен по причине: `{причина}`"
+)
+
+    await interaction.response.send_message(embed=embed)
+
+@tree.command(name = "timeout", description = "Заглушить участника")
+@app_commands.default_permissions(moderate_members=True)
+async def self(interaction: discord.Interaction, участник:discord.Member, минуты: int, причина:str):
+    delta = timedelta(minutes=минуты)
+    await участник.timeout(delta, reason=причина)
+    embed = discord.Embed(
+    colour=discord.Colour.teal(),
+    description= f":white_check_mark: **{участник}** был отправлен в мут на {минуты} минуты по причине: `{причина}`"
+)
+
+    await interaction.response.send_message(f"{участник.mention}", embed=embed)
+
+@tree.command(name = "warn", description = "Отправить предупреждение")
+@app_commands.default_permissions(moderate_members=True)
+async def self(interaction: discord.Interaction, участник:discord.Member, *, причина:str):
+    embed = discord.Embed(
+    colour=discord.Colour.teal(),
+    description= f":white_check_mark: **{участник}** получил предупреждение по причине: `{причина}`"
+)
+
+    await interaction.response.send_message(f"{участник.mention}", embed=embed)
 
 client.run(config.TOKEN)
