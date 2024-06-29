e6adb10a22b548a32309fca688c1f90ac80ebb1a
Art5507 <arturbakiev028@gmail.com>
2024-06-30 00:23:52 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -48,14 +48,15 @@ async def self(interaction: discord.Interaction):
 async def self(interaction: discord.Interaction):
     embed = discord.Embed(
         colour=discord.Colour.teal(),
-        description=f"## Справка Топтунового бота\n### Команды\nВведите `/`, чтобы просмотреть список команд или [просмотрите документацию](https://tptnbot.vercel.app/docs/category/%D1%82%D0%BE%D0%BF%D1%82%D1%83%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9-%D0%B1%D0%BE%D1%82).\n### Документация\nУзнайте о том, как использовать Топтунового бота на вашем сервере. [Просмотрите документацию](https://tptnbot.vercel.app/docs/category/%D1%82%D0%BE%D0%BF%D1%82%D1%83%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9-%D0%B1%D0%BE%D1%82).\n### Сервер поддержки\nУ вас возникла проблема, есть предложение или вы просто хотите получать уведомления о новых возможностях? [Вступайте в наш Discord сервер](https://discord.gg/BCp784Gr3x).",
+        description=f"## Справка Топтунового бота\n### Команды\nВведите `/`, чтобы просмотреть список команд или [просмотрите документацию](https://tptnbot.vercel.app/docs/category/%D1%82%D0%BE%D0%BF%D1%82%D1%83%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9-%D0%B1%D0%BE%D1%82).\n### Документация\nУзнайте о том, как использовать Топтунового бота на вашем сервере. [Просмотрите документацию](https://tptnbot.vercel.app/docs/category/%D1%82%D0%BE%D0%BF%D1%82%D1%83%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9-%D0%B1%D0%BE%D1%82).\n### Сервер поддержки\nУ вас возникла проблема, есть предложение или вы просто хотите получать уведомления о новых возможностях? [Вступайте в наш Discord сервер](https://discord.gg/BCp784Gr3x).\n### Telegram-канал\nХотите получать уведомления о новых возможностях в боте? [Вступайте в наш Telegram-канал](https://t.me/ToptunovyjBot).",
     )
-    embed.set_thumbnail(url="https://i.imgur.com/LkSe8du.jpeg")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/1255129815418142740/1255142429426974791/photo_2024-05-16_18-00-01.jpg?ex=668153fa&is=6680027a&hm=5b9dc5c33a653bb84124c7bd067061ca20211b71375b07f8c7c4d2d0b80f2d8a&")
+    embed.set_footer(text="Сделано с ❤️ от Art5507 • https://tptnbot.vercel.app/", icon_url="https://media.discordapp.net/attachments/1255129815418142740/1255143466346876928/29891-ezgif.com-crop.png?ex=668154f1&is=66800371&hm=f04366480937c32c5029d442d8729d9b9eb4cbc15a11663ee571b19bcd436500&")
 
     await interaction.response.send_message(embed=embed)
 
 # Команда /random
-@tree.command(name = "random", description = "Отправляет рандомное число")
+@tree.command(name = "random", description = "Отправляет рандомное число от 1 до 999")
 async def self(interaction: discord.Interaction):
     random_number = random.randint(1, 999)
     await interaction.response.send_message(f"{random_number}")
@@ -84,70 +85,79 @@ async def self(interaction: discord.Interaction):
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} ушел в афк: {сообщение}")
+    await interaction.response.send_message(f":sailboat: {interaction.user.mention} ушел в афк: {сообщение}")
 
 # Команда /gn
 @tree.command(name = "gn", description = "Перейти в режим сна")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} ушел спать: {сообщение}")
+    await interaction.response.send_message(f":zzz: {interaction.user.mention} ушел спать: {сообщение}")
 
 # Команда /brb
 @tree.command(name = "brb", description = "Перейти в режим скоро вернусь")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} скоро вернется: {сообщение}")
+    await interaction.response.send_message(f"<a:ppHop:1256667376599171113> {interaction.user.mention} скоро вернется: {сообщение}")
 
 # Команда /poop
 @tree.command(name = "poop", description = "Перейти в режим какания")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас какает: {сообщение}")
+    await interaction.response.send_message(f":toilet: {interaction.user.mention} сейчас какает: {сообщение}")
 
 # Команда /shower
 @tree.command(name = "shower", description = "Перейти в режим принятия душа")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} принимает душ: {сообщение}")
+    await interaction.response.send_message(f":shower: {interaction.user.mention} принимает душ: {сообщение}")
 
 # Команда /food
 @tree.command(name = "food", description = "Перейти в режим приема пищи")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас ест: {сообщение}")
+    food_list = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🫐", "🥝", "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️", "🫑", "🥒", "🥬", "🥦", "🧄", "🧅", "🥜", "🫘", "🌰", "🫚", "🫛", "🍞", "🥐", "🥖", "🫓", "🥨", "🥯", "🥞", "🧇", "🧀", "🍖", "🍗", "🥩", "🥓", "🍔", "🍟", "🍕", "🌭", "🥪", "🌮", "🌯", "🫔", "🥙", "🧆", "🥚", "🍳", "🥘", "🍲", "🫕", "🥣", "🥗", "🍿", "🧈", "🧂", "🥫", "🍱", "🍘", "🍙", "🍚", "🍛", "🍜", "🍝", "🍠", "🍢", "🍣", "🍤", "🍥", "🥮", "🍡", "🥟", "🥠", "🥡", "🦀", "🦞", "🦐", "🦑", "🦪", "🍦", "🍧", "🍨", "🍩", "🍪", "🎂", "🍰", "🧁", "🥧", "🍫", "🍬", "🍭", "🍮", "🍯", "🍼", "🥛", "☕", "🫖", "🍵", "🍶", "🍾", "🍷", "🍸", "🍹", "🍺", "🍻", "🥂", "🥃", "🫗", "🥤", "🧋", "🧃", "🧉", "🧊", "🥢", "🍽️", "🍴", "🥄", "🔪", "🫙", "🏺"]
+    random_food = random.choice(food_list)
+    await interaction.response.send_message(f"{random_food} {interaction.user.mention} сейчас ест: {сообщение}")
 
 # Команда /lurk
 @tree.command(name = "lurk", description = "Перейти в режим скрытности")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас скрывается: {сообщение}:")
+    await interaction.response.send_message(f":busts_in_silhouette: {interaction.user.mention} сейчас скрывается: {сообщение}")
 
 # Команда /work
 @tree.command(name = "work", description = "Перейти в рабочий режим")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} работает: {сообщение}")
+    await interaction.response.send_message(f":briefcase: {interaction.user.mention} работает: {сообщение}")
 
 # Команда /study
 @tree.command(name = "study", description = "Перейти в режим обучения")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас учится: {сообщение}")
+    await interaction.response.send_message(f":books: {interaction.user.mention} сейчас учится: {сообщение}")
+
+# Команда /draw
+@tree.command(name = "draw", description = "Перейти в режим рисования")
+async def self(interaction: discord.Interaction, сообщение: str = None):
+    if сообщение == None:
+        сообщение = "(нет сообщения)"
+    await interaction.response.send_message(f":paintbrush: {interaction.user.mention} сейчас рисует: {сообщение}")
 
 # Команда /nap
 @tree.command(name = "nap", description = "Перейти в режим дремоты")
 async def self(interaction: discord.Interaction, сообщение: str = None):
     if сообщение == None:
         сообщение = "(нет сообщения)"
-    await interaction.response.send_message(f"{interaction.user.mention} сейчас дремлет: {сообщение}")
+    await interaction.response.send_message(f":sleeping: {interaction.user.mention} сейчас дремлет: {сообщение}")
 
 # Команда /tuck
 @tree.command(name = "tuck", description = "Уложить человека спать")
@@ -176,7 +186,9 @@ async def self(interaction: discord.Interaction, количество:int):
 # Команда /kick
 @tree.command(name = "kick", description = "Выгнать участника")
 @app_commands.default_permissions(kick_members=True)
-async def self(interaction: discord.Interaction, участник:discord.Member, *, причина:str):
+async def self(interaction: discord.Interaction, участник:discord.Member, *, причина: str = None):
+    if причина == None:
+        причина = "(причина не указана)"
     await участник.kick(reason=причина)
     embed1 = discord.Embed(
         colour=discord.Colour.teal(),
@@ -194,7 +206,9 @@ async def self(interaction: discord.Interaction, участник:discord.Member
 # Команда /ban
 @tree.command(name = "ban", description = "Забанить участника")
 @app_commands.default_permissions(ban_members=True)
-async def self(interaction: discord.Interaction, участник:discord.Member, *, причина:str):
+async def self(interaction: discord.Interaction, участник:discord.Member, *, причина: str = None):
+    if причина == None:
+        причина = "(причина не указана)"
     await участник.ban(reason=причина, delete_message_days=1)
     embed1 = discord.Embed(
         colour=discord.Colour.teal(),
@@ -212,7 +226,9 @@ async def self(interaction: discord.Interaction, участник:discord.Member
 # Команда /timeout
 @tree.command(name = "timeout", description = "Заглушить участника")
 @app_commands.default_permissions(moderate_members=True)
-async def self(interaction: discord.Interaction, участник:discord.Member, минуты: int, причина:str):
+async def self(interaction: discord.Interaction, участник:discord.Member, минуты: int, причина: str = None):
+    if причина == None:
+        причина = "(причина не указана)"
     delta = timedelta(minutes=минуты)
     await участник.timeout(delta, reason=причина)
     embed1 = discord.Embed(
@@ -231,7 +247,9 @@ async def self(interaction: discord.Interaction, участник:discord.Member
 # Команда /warn
 @tree.command(name = "warn", description = "Отправить предупреждение")
 @app_commands.default_permissions(moderate_members=True)
-async def self(interaction: discord.Interaction, участник:discord.Member, *, причина:str):
+async def self(interaction: discord.Interaction, участник:discord.Member, *, причина: str = None):
+    if причина == None:
+        причина = "(причина не указана)"
     embed1 = discord.Embed(
         colour=discord.Colour.teal(),
         description= f":white_check_mark: **{участник}** получил предупреждение по причине: `{причина}`"
@@ -297,6 +315,10 @@ async def on_message(message=discord.Message):
       await message.add_reaction("\U0001F44E")
     if message.channel.id == 1208840805205414018: # Сервер tipo_Lon (музыка)
       await message.create_thread(name="Комментарии")
+      await message.add_reaction("\U0001F44D")
+      await message.add_reaction("\U0001F44E")
+    if message.channel.id == 1256572605088006144: # Сервер tipo_Lon (клипы)
+      await message.create_thread(name="Комментарии") 
       await message.add_reaction("\U0001F44D")
       await message.add_reaction("\U0001F44E")
 
